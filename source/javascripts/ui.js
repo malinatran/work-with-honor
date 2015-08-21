abbda7f59ed13059feaee2afe502017710f9718f
Pablo Fierro <hello@pablofierro.me>
2015-08-21 09:05:53 -0500

    Progress

--- source/javascripts/ui.js
+++ source/javascripts/ui.js
@@ -3,10 +3,36 @@
  */
 $(function() {
 
+  /**
+   * Adapt each slider to defined styles
+   */
+  function onSliderBefore(e) {
+    var $el = $(e.in.el);
+    var $parent = $el.parents('.section');
+    var color = $el.data('color');
+    var fontColor = $el.data('font-color');
+    if($parent) {
+      $parent.css({
+        background: color || '',
+        color: fontColor || ''
+      });
+    }
+  }
+
+  /**
+   * Initialize page sliders
+   */
   $('.slider').each(function(index, slider) {
     var id = 'slider-' + index;
-    $(slider).attr('id', id);
+    var $slider = $(slider);
+    $slider.attr('id', id);
+    var slidesCycle = [];
+    $slider.find('.slide').each(function(slideIndex) {
+      slidesCycle.push(slideIndex + 1 + '');
+    });
+    slidesCycle.push(slidesCycle[0]);
     slidr.create(id, {
+      before: onSliderBefore,
       controls: 'none',
       breadcrumbs: true,
       direction: 'horizontal',
@@ -18,7 +44,23 @@ $(function() {
       timing: { 'cube': '0.5s ease-in' },
       touch: true,
       transition: 'fade'
-    }).start();
+    }).add('h', slidesCycle).auto($slider.data('timer') || 5000);
+  });
+
+  /**
+   * Modals
+   */
+  $('a[data-modal]').on('click', function() {
+    var $target = $('#modal-' + $(this).data('modal'));
+    if($target) {
+      $(document.body).addClass('noscroll');
+      $target.addClass('visible');
+    }
+  });
+
+  $('.modal a.close').on('click', function() {
+    $(this).parents('.modal').removeClass('visible');
+    $(document.body).removeClass('noscroll');
   });
 
 });
