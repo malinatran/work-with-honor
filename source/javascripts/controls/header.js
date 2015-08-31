ead8ff4fd8d6639db44b7c4b7e9b83b12a5647da
pablofierro <hello@pablofierro.me>
2015-08-30 22:31:19 -0500

    Add header animation

---
+++ source/javascripts/controls/header.js
@@ -0,0 +1,34 @@
+/**
+ * Sliders
+ */
+$(function() {
+
+  /**
+   * Sticky header
+   */
+  var $header = $('header');
+  var $document = $(document);
+  var $floatHeader = $('#floatmenu');
+  var headerHeight = $header.outerHeight();
+  var floatHeaderHeight = $floatHeader.height();
+  var isScrollBelowHeader = false;
+  var scrollTop = 0;
+
+  function onScroll() {
+    if(isScrollBelowHeader && $document.scrollTop() - scrollTop < 0) {
+      $header.addClass('sticky');
+    }
+    else {
+      $header.removeClass('sticky');
+    }
+    scrollTop = $document.scrollTop();
+    if(scrollTop > headerHeight) {
+      isScrollBelowHeader = true;
+    }
+    else {
+      isScrollBelowHeader = false;
+    }
+  }
+  $(document).on('scroll', onScroll);
+
+});
