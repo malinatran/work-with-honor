fb1ebe8a3f01a3f0361b624ab4b78b0eb56a9777
pablofierro <hello@pablofierro.me>
2015-08-30 20:41:06 -0500

    Add contact form functioanlity

---
+++ source/javascripts/controls/select.js
@@ -0,0 +1,81 @@
+/**
+ * Sliders
+ */
+$(function() {
+
+  // current select
+  var currentSelect = null;
+
+  // close current select
+  function closeCurrentSelect() {
+    if(currentSelect === null) {
+      return;
+    }
+    $(document.body).removeClass('nooverflow');
+    currentSelect.removeClass('active');
+    document.removeEventListener('click', onDocumentClick, true);
+  }
+
+  // on document click
+  function onDocumentClick(event) {
+    var $dropdown = currentSelect.find('ul.dropdown');
+    if(currentSelect.has(event.target).length === 0) {
+      closeCurrentSelect();
+    }
+  }
+
+  // on select click
+  function onSelectClick(event) {
+    var $this = $(this);
+    var $target = $(event.target)
+
+    // close current select box
+    if(currentSelect !== null && currentSelect.attr('id') !== $this.attr('id')) {
+      closeCurrentSelect();
+    }
+
+    currentSelect = $this;
+
+    // on item click
+    if($target.is('li')) {
+      currentSelect.find('select').val($target.html());
+      currentSelect.find('.value').html($target.html());
+      currentSelect.find('label.error').remove();
+      closeCurrentSelect();
+    }
+    else {
+      currentSelect.toggleClass('active');
+      if(currentSelect.hasClass('active')) {
+        $(document.body).addClass('nooverflow');
+        document.addEventListener('click', onDocumentClick, true);
+      }
+    }
+
+  }
+
+  /**
+   * Select form control
+   */
+  $('.field .select').each(function(index, select) {
+    var $dropdown = null;
+    var $this = $(this);
+    var $select = $this.find('select');
+    $select.addClass('activated');
+    $this.prop('id', 'select-' + index);
+
+    // copy avaialable options
+    var children = $select.find('option').map(function() {
+      if($(this).prop('value') == '') {
+        return null;
+      }
+      return '<li>' + $(this).html() + '</li>';
+    });
+    $this.append('<ul class="dropdown">' + children.get().join('') + '</ul>');
+    $dropdown = $this.find('ul.dropdown');
+
+    // bind click event
+    $this.on('click', onSelectClick);
+
+  });
+
+});
--- source/javascripts/ui.js
+++ source/javascripts/ui.js
@@ -58,9 +58,54 @@ $(function() {
     }
   });
 
+  // modal close
   $('.modal a.close').on('click', function() {
     $(this).parents('.modal').removeClass('visible');
     $(document.body).removeClass('noscroll');
+  });
+
+  // form controls
+  $('form').each(function(index, form) {
+    var $form = $(form);
+    $form.attr('id', 'form-' + index);
+    $form.find('input').addClass('empty');
+    $form.on('input', 'input', function() {
+      if($(this).val()) {
+        $(this).removeClass('empty').addClass('filled');
+      }
+      else {
+        $(this).addClass('empty');
+      }
+    })
+    $form.validate({
+      submitHandler: function(form, event) {
+        event.preventDefault();
+        var $form = $(form);
+        var action = $form.prop('action');
+        var buttonMessage = $form.data('buttonMessage') || null;
+        if($form.data('submitting')) {
+          return;
+        }
+        $form.data('submitting', true);
+        $.post(action, $form.serialize(), function() {
+          if(buttonMessage) {
+            $form.find('button[type=submit]').addClass('aqua').html(buttonMessage);
+            $form.find('.success').removeClass('hidden');
+          }
+          $form.data('submitting', false);
+        });
+      }
+    });
+  });
+
+  // options form control
+  $('.options').on('click', '.option', function() {
+    var $this = $(this);
+    var $parent = $(this).parents('.options');
+    $parent.find('.option').removeClass('active');
+    $this.addClass('active');
+    $parent.find('input').val($this.data('value'));
+    $parent.find('label.error').remove();
   });
 
 });
